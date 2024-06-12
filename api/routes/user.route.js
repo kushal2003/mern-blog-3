512b617eafd96c24d73c8b3096e53b2415385fe0
kushal2003 <kucoolsh@gmail.com>
2024-06-12 23:59:35 +0530

    Almost final

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { test } from '../controllers/user.controller.js';
+import { getUser, test } from '../controllers/user.controller.js';
 import { updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 import { deleteUser } from '../controllers/user.controller.js';
 import { signout } from '../controllers/user.controller.js';
 import { getUsers } from '../controllers/user.controller.js';
+
 
 const router = express.Router();
 
@@ -13,6 +14,7 @@ router.put('/update/:userId',verifyToken,updateUser);
 router.delete('/delete/:userId',verifyToken,deleteUser);
 router.post('/signout',signout);
 router.get('/getUsers',verifyToken,getUsers);
+router.get('/:userId',getUser);
 
 
 export default router;
