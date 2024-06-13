ed16afa0645b321d21d3f2015d516f72da4d92af
kushal2003 <kucoolsh@gmail.com>
2024-06-13 10:20:46 +0530

    comment

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -5,6 +5,7 @@ import { getPostComments } from "../controllers/comment.controller.js";
 import { likeComment } from "../controllers/comment.controller.js";
 import { editComment } from "../controllers/comment.controller.js";
 import {deleteComment} from "../controllers/comment.controller.js"
+import { getcomments } from "../controllers/comment.controller.js";
 const router = express.Router();
 
 router.post('/create',verifyToken,createComment);
@@ -12,6 +13,7 @@ router.get('/getPostComments/:postId',getPostComments);
 router.put('/likeComment/:commentId',verifyToken,likeComment);
 router.put('/editComment/:commentId',verifyToken,editComment)
 router.delete('/deleteComment/:commentId',verifyToken,deleteComment)
+router.get('/getcomments',getcomments);
 
 
 
